936501174932fb12cdcab8ca7a100053eee72d6d
InvXander <113646174+Xantech007@users.noreply.github.com>
2026-08-14 11:23:43 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,14 +5,13 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebas
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDVK9uMHRCbxOkO7U696v7runiRB0MNXi0",
-  authDomain: "gameware-emma.firebaseapp.com",
-  projectId: "gameware-emma",
-  storageBucket: "gameware-emma.firebasestorage.app",
-  messagingSenderId: "476632021558",
-  appId: "1:476632021558:web:d27c0bfe7ad590e9ca7697",
+  apiKey: "AIzaSyAErOMhI2wWc7gyFlExOb7DXLgSsAlQW3Y",
+  authDomain: "online-gamer-me.firebaseapp.com",
+  projectId: "online-gamer-me",
+  storageBucket: "online-gamer-me.firebasestorage.app",
+  messagingSenderId: "717118566143",
+  appId: "1:717118566143:web:f227c8a43a2c1ad2d0e6ca"
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
